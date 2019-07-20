052b7bd10a91aea791ca6f5da19c67c4ae681c47
Joel Velis <joelvelis94@gmail.com>
2019-07-20 11:53:52 -0400

    create ability to add two lists

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+function AddPerson(props) {
+    return (
+            <button className='addPerson' onClick={props.onClicked}>Add Person</button>
+    );
+}
+
+function PersonList(props) {
+    return (
+        <div>
+        {props.list.map((name) => (
+            <span key={name}>{name}</span>
+        ))}
+        </div>
+    );
+};
+
 class Details extends React.Component {
     constructor(props) {
         super(props); 
@@ -12,7 +28,9 @@ class Details extends React.Component {
                 tax: 0,
                 tip: 0,
                 grand_total: 0,
-            }
+                clicked: [],
+            };
+            this.handleClick = this.handleClick.bind(this)
     }
 
     handleMealChange = event => {
@@ -42,21 +60,41 @@ class Details extends React.Component {
         )
     }
 
+    renderAddPerson = () => {
+        return (
+            <AddPerson 
+                onClicked= {() => this.handleClick()}    
+                />
+        );
+    }
+
+    handleClick = () => {
+        let clickedPlus = this.state.clicked.concat(this.renderPerson())
+        this.setState({
+            clicked: clickedPlus,
+        });
+        this.state.clicked.length < 10 ? console.log(this.state.clicked) : this.renderPerson() 
+    }
+
     render() {
         let grand_total = parseFloat(this.state.meal_total) * (1 + parseFloat(this.state.tax)/100) * (1 + parseFloat(this.state.tip)/100);
         return (
             <div className='details'>
                 <form>
                     <label htmlFor='meal'>Meal total: </label>
-                    <input name='meal' value={this.state.meal_total} onChange={this.handleMealChange}></input><br></br>
+                    <input name='meal' placeholder={this.state.meal_total} onChange={this.handleMealChange}></input><br></br>
                     <label htmlFor='tax'>Tax: </label>
-                    <input name='tax' value={this.state.tax} onChange={this.handleTaxChange}></input><br></br>
+                    <input name='tax' placeholder={this.state.tax} onChange={this.handleTaxChange}></input><br></br>
                     <label htmlFor='tip'>Tip: </label>
-                    <input name='tip' value={this.state.tip} onChange={this.handleTipChange}></input><br></br>
+                    <input name='tip' placeholder={this.state.tip} onChange={this.handleTipChange}></input><br></br>
                     <label htmlFor='total'>Grand Total:</label>
                     <input name='total' value={grand_total} readOnly></input><br></br>
                 </form>
                 {this.renderPerson()}
+                <PersonList list={this.state.clicked} />
+                {this.renderAddPerson()}
+                
+                
             </div>
         );
     }
@@ -66,7 +104,7 @@ class Person extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            person_meal_total: 0,
+            person_meal_total: [],
             person_total: 0,
         }
     }
@@ -96,27 +134,11 @@ class Person extends React.Component {
     }
 }
 
-class AddPerson extends React.Component {
-    renderPerson() {
-        return (
-            <Person/>
-        )
-    }
-    render() {
-        return (
-            
-                <button className='addPerson' onClick={() => this.renderPerson()}>Add Person</button>
-            
-        )
-    }
-}
-
 class Calculator extends React.Component {
     render() {
         return (
             <div>
                 <Details/>
-                <AddPerson/>
             </div>
         )
     }
