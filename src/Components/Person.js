e53d10b849dc1c543fe8e3fcaa7d3210798c4cfc
Joel Velis <joelvelis94@gmail.com>
2019-07-31 11:46:19 -0400

    connected one instance of item to everything else

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Items from './Items.js'
 
 class Person extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            person_meal_subtotal: 0,
+            item: 0,
+            item_quantity: 0
         }
     }
 
-    handlePersonSubtotal = event => {
+    handleInput = event => {
+        const target_value = event.target.value
+        const name = event.target.name
         this.setState({
-            person_meal_subtotal: event.target.value
+            [name]: target_value,
         });
     };
 
@@ -18,22 +22,25 @@ class Person extends React.Component {
     
     render() {
         let shared_total = parseFloat(this.props.shared_items) / this.props.diners +  (parseFloat(this.props.shared_items) / this.props.diners * (this.props.tax/100)) + (parseFloat(this.props.shared_items) / this.props.diners * (this.props.tip/100))
-        let person_total = parseFloat(this.state.person_meal_subtotal) + shared_total + ((parseFloat(this.state.person_meal_subtotal)) * (parseFloat(this.props.tax)/100)) + ((parseFloat(this.state.person_meal_subtotal)) * (parseFloat(this.props.tip)/100));
+        let item_total = parseFloat(this.state.item) * parseFloat(this.state.item_quantity)
+        let person_total = parseFloat(item_total) + shared_total + ((parseFloat(item_total)) * (parseFloat(this.props.tax)/100)) + ((parseFloat(item_total)) * (parseFloat(this.props.tip)/100));
         return (
             <div className='person'>
                 <div className='total-details'>
                     <h3>Person {this.props.person_num} </h3>
-                    <form>
-                        <label htmlFor='person-meal'>Personal Subtotal: $ </label>
-                        <input name='person-meal' value={this.state.person_meal_subtotal} onChange={this.handlePersonSubtotal}></input>
-                    </form>
+                    <Items 
+                        item = {this.state.item}
+                        item_quantity = {this.state.item_quantity}
+                        handleInput = {this.handleInput}
+                    />
+                    <p>Person Subtotal: ${item_total}</p><br></br>
                 </div>
                 <div className='breakdown'>
                     <h3>Should Pay</h3>
                     <div className='person-details'>
                         <p className='tooltip'>Shared: ${shared_total.toFixed(2)}<span className='tooltiptext'>tax is included in this amount</span></p><br></br>
-                        <p>Tax: ${((parseFloat(this.props.tax)/100) * parseFloat(this.state.person_meal_subtotal)).toFixed(2)}</p><br></br>
-                        <p>Tip: ${((parseFloat(this.props.tip)/100) * parseFloat(this.state.person_meal_subtotal)).toFixed(2)}</p><br></br>
+                        <p>Tax: ${((parseFloat(this.props.tax)/100) * parseFloat(item_total)).toFixed(2)}</p><br></br>
+                        <p>Tip: ${((parseFloat(this.props.tip)/100) * parseFloat(item_total)).toFixed(2)}</p><br></br>
                         <p>Total: ${person_total.toFixed(2)}</p>
                     </div>
                 </div>    
