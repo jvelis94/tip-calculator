18279dd899ef2a2231115624b76687fcccab4677
Joel Velis <joelvelis94@gmail.com>
2019-07-31 12:53:04 -0400

    added ability top add multiple items per person, with small bugs

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 export default function Items(props){
-    let item_total = parseFloat(props.item) * parseFloat(props.item_quantity)
         return (
             <div>
                 <form>
                     Item<input name='item' placeholer='0' onChange={props.handleInput}></input>
                     Item Quantity<input name='item_quantity' placeholer='0' onChange={props.handleInput}></input>
-                    <p style={{display: 'inline'}}>Item Total: ${item_total}</p>
+
                 </form>
-                
             </div>
         )
 }
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -6,7 +6,10 @@ class Person extends React.Component {
         super(props);
         this.state = {
             item: 0,
-            item_quantity: 0
+            item_quantity: 0,
+            items_counter: 0,
+            item_totals: [],
+            items_totals_sum: 0
         }
     }
 
@@ -18,29 +21,51 @@ class Person extends React.Component {
         });
     };
 
-    
+    handleAddItemBtn = () => {
+        let items_counter = this.state.items_counter + 1
+        let item_totals = this.state.item_totals.concat([this.renderItemTotal()])
+        const reducer = (accumulator, currentValue) => accumulator + currentValue
+        let items_totals_sum = item_totals.reduce(reducer)
+        this.setState({
+            items_counter: items_counter,
+            item_totals: item_totals,
+            items_totals_sum: items_totals_sum
+        })
+    }
+
+    renderItemTotal = () => {
+        return parseFloat(this.state.item) * parseFloat(this.state.item_quantity)
+    }
     
     render() {
         let shared_total = parseFloat(this.props.shared_items) / this.props.diners +  (parseFloat(this.props.shared_items) / this.props.diners * (this.props.tax/100)) + (parseFloat(this.props.shared_items) / this.props.diners * (this.props.tip/100))
-        let item_total = parseFloat(this.state.item) * parseFloat(this.state.item_quantity)
-        let person_total = parseFloat(item_total) + shared_total + ((parseFloat(item_total)) * (parseFloat(this.props.tax)/100)) + ((parseFloat(item_total)) * (parseFloat(this.props.tip)/100));
+        let person_total = parseFloat(this.state.items_totals_sum) + shared_total + ((parseFloat(this.state.items_totals_sum)) * (parseFloat(this.props.tax)/100)) + ((parseFloat(this.state.items_totals_sum)) * (parseFloat(this.props.tip)/100));
+        let items = []
+        let i = 0
+        while (i < this.state.items_counter) {
+            items.push(
+                <Items 
+                        item = {this.state.item}
+                        item_quantity = {this.state.item_quantity}
+                        handleInput = {this.handleInput}
+                />
+            )
+            i++
+        }
         return (
             <div className='person'>
                 <div className='total-details'>
                     <h3>Person {this.props.person_num} </h3>
-                    <Items 
-                        item = {this.state.item}
-                        item_quantity = {this.state.item_quantity}
-                        handleInput = {this.handleInput}
-                    />
-                    <p>Person Subtotal: ${item_total}</p><br></br>
+                    {items}
+                    <button onClick = {this.handleAddItemBtn}>Add Item</button>
+                    <p>Person Subtotal: ${this.state.items_totals_sum}</p><br></br>
                 </div>
                 <div className='breakdown'>
                     <h3>Should Pay</h3>
                     <div className='person-details'>
                         <p className='tooltip'>Shared: ${shared_total.toFixed(2)}<span className='tooltiptext'>tax is included in this amount</span></p><br></br>
-                        <p>Tax: ${((parseFloat(this.props.tax)/100) * parseFloat(item_total)).toFixed(2)}</p><br></br>
-                        <p>Tip: ${((parseFloat(this.props.tip)/100) * parseFloat(item_total)).toFixed(2)}</p><br></br>
+                        <p>Tax: ${((parseFloat(this.props.tax)/100) * parseFloat(this.state.items_totals_sum)).toFixed(2)}</p><br></br>
+                        <p>Tip: ${((parseFloat(this.props.tip)/100) * parseFloat(this.state.items_totals_sum)).toFixed(2)}</p><br></br>
                         <p>Total: ${person_total.toFixed(2)}</p>
                     </div>
                 </div>    
